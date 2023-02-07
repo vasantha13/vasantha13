7dd69d2df3b7e447bd33576f604c4ca6f8ffc0c4
root <root@ip-172-31-44-5.eu-west-2.compute.internal>
2023-02-07 15:17:57 +0000

    java code

---
+++ demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+  public static void main(...)
+  {
+     system.out.print(" Good day ");
+     system.out.print(" Good day ");
+     system.out.print(" Good day ");
+     system.out.print(" Good day ");
+  }
+}
